a56f01add32fccadc92becdd557a7804bdfad1b6
maijiazhao <maijiazhao@cvte.com>
2020-07-30 11:23:57 +0800

    feat(页面): 创建主页、用户配置页

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,32 @@
+import React, { memo } from 'react';
+
+//私有常量
+
+//可抽离的逻辑处理函数/组件
+
+let HomePage = (props: IProps) => {
+    //变量声明、解构
+
+    //组件状态
+
+    //网络IO
+
+    //数据转换
+
+    //逻辑处理函数
+
+    //组件Effect
+
+    return (
+        <div>首页</div>
+    );
+};
+
+//props类型定义
+interface IProps {
+    
+}
+
+//prop-type定义，可选
+
+export default memo(HomePage);
---
+++ src/pages/UserSettingPage.tsx
@@ -0,0 +1,34 @@
+import React, { memo } from 'react';
+import UserSetting from '../Components/UserSetting';
+
+//私有常量
+
+//可抽离的逻辑处理函数/组件
+
+let UserSettingPage = (props: IProps) => {
+    //变量声明、解构
+
+    //组件状态
+
+    //网络IO
+
+    //数据转换
+
+    //逻辑处理函数
+
+    //组件Effect
+
+    return (
+        <div>
+            <UserSetting />
+        </div>
+    )
+};
+
+//props类型定义
+interface IProps {
+    
+}
+
+//prop-type定义，可选
+export default memo(UserSettingPage);
